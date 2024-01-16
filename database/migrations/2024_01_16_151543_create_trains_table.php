5122205e4ba0b1c7101b6486d2f2fb30f8d49922
valerioperria <valerio.perria@gmail.com>
2024-01-16 16:58:49 +0100

    feat. template; milestone_1

---
+++ database/migrations/2024_01_16_151543_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->mediumInteger('train_code');
+            $table->tinyInteger('carriages_number');
+            $table->boolean('on_time')->default(1);
+            $table->tinyInteger('delay')->default(0);
+            $table->boolean('delete')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
